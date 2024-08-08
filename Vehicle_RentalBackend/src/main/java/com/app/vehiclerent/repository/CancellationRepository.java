9b2834f2c78e9370f50cdeb412d89f775f46f301
SANKET-GHAYTADAK <sanketghaytadak07@gmail.com>
2024-08-08 14:15:25 +0530

    initial commit

--- Vehicle_RentalBackend/src/main/java/com/app/vehiclerent/repository/CancellationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.app.vehiclerent.repository;
-
-import com.app.vehiclerent.entity.Cancellation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CancellationRepository extends JpaRepository<Cancellation, Long> {
-	
-}
